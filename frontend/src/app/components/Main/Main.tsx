1a579ecd6138ec34ab3206381e53a138ff57e8ee
Roland <machodin@yandex.ru>
2025-03-13 02:34:25 +0300

    fix

--- frontend/src/app/components/Main/Main.tsx
+++ frontend/src/app/components/Main/Main.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import styles from './Main.module.scss';
 import carbonDocImg from '../../../../public/carbon_document-signed.svg';
