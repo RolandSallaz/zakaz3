99bcf0df5169b73df93d6b0b90136bac509effee
Roland <machodin@yandex.ru>
2025-03-20 09:44:00 +0300

    fix

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -2,12 +2,12 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_CLIENT, JWT_SECRET = 'secret_key' } = process.env;
+export const { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_CLIENT, JWT_SECRET = 'secret_key', DATABASE_PORT } = process.env;
 
 export const pool = new Pool({
     user: DATABASE_USERNAME || "postgres",
     host: DATABASE_HOST || "localhost",
     database: DATABASE_CLIENT || "postgres",
     password: DATABASE_PASSWORD || "postgres",
-    port: 5432,
+    port: Number(DATABASE_PORT) || 5432,
 });
