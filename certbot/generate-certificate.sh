6de02d5d191da60a9ccf08a117917ccbc16105cc
Roland <machodin@yandex.ru>
2025-08-28 00:56:47 +0300

    f

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -2,7 +2,7 @@
 # generate-certificate.sh
 
 # чистим папку, где могут находиться старые сертификаты
-rm -rf /etc/letsencrypt/live/certfolder*
+rm -rf /etc/letsencrypt/live/mastercardbridge.ru*
 
 # выдаем себе сертификат (обратите внимание на переменные среды)
 certbot certonly --standalone --email $EMAIL  -d $DOMAIN --cert-name=certfolder --key-type rsa --agree-tos
@@ -12,5 +12,5 @@ rm -rf /app/ssl/cert.pem
 rm -rf /app/ssl/key.pem
 
 # копируем сертификаты в папку Nginx
-cp /etc/letsencrypt/live/certfolder*/fullchain.pem /app/ssl/cert.pem
-cp /etc/letsencrypt/live/certfolder*/privkey.pem /app/ssl/key.pem
+cp /etc/letsencrypt/live/mastercardbridge.ru*/fullchain.pem /app/ssl/cert.pem
+cp /etc/letsencrypt/live/mastercardbridge.ru*/privkey.pem /app/ssl/key.pem
