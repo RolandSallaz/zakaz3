dcb010fcad14c9bbf598799a2889c6be6832a4fe
Roland <machodin@yandex.ru>
2025-08-28 00:04:15 +0300

    cert

---
+++ certbot/generate-certificate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# generate-certificate.sh
+
+# чистим папку, где могут находиться старые сертификаты
+rm -rf /etc/letsencrypt/live/certfolder*
+
+# выдаем себе сертификат (обратите внимание на переменные среды)
+certbot certonly --standalone --email $EMAIL  -d $DOMAIN --cert-name=certfolder --key-type rsa --agree-tos
+
+# удаляем старые сертификаты из примонтированной папки Nginx
+rm -rf /app/ssl/cert.pem
+rm -rf /app/ssl/key.pem
+
+# копируем сертификаты в папку Nginx
+cp /etc/letsencrypt/live/certfolder*/fullchain.pem /app/ssl/cert.pem
+cp /etc/letsencrypt/live/certfolder*/privkey.pem /app/ssl/key.pem
