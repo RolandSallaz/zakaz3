699829612c9f34fe57591acdc45227e7e8fd6992
Roland <machodin@yandex.ru>
2025-11-28 17:00:50 +0300

    fix

--- certbot/generate-certificate.sh
+++ certbot/generate-certificate.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # generate-certificate.sh
 
-# чистим папку, где могут находиться старые сертификаты
-rm -rf /etc/letsencrypt/live/mastercardbridge.ru*
+# Выдаем сертификат с принудительным обновлением
+certbot certonly --standalone --email $EMAIL -d $DOMAIN --cert-name=certfolder --key-type rsa --agree-tos --force-renewal
 
-# выдаем себе сертификат (обратите внимание на переменные среды)
-certbot certonly --standalone --email $EMAIL  -d $DOMAIN --cert-name=certfolder --key-type rsa --agree-tos
-
-# удаляем старые сертификаты из примонтированной папки Nginx
+# Удаляем старые сертификаты из примонтированной папки Nginx
 rm -rf /app/ssl/cert.pem
 rm -rf /app/ssl/key.pem
 
-# копируем сертификаты в папку Nginx
-cp /etc/letsencrypt/live/mastercardbridge.ru*/fullchain.pem /app/ssl/cert.pem
-cp /etc/letsencrypt/live/mastercardbridge.ru*/privkey.pem /app/ssl/key.pem
+# Копируем сертификаты в папку Nginx (используем конкретное имя)
+cp /etc/letsencrypt/live/certfolder/fullchain.pem /app/ssl/cert.pem
+cp /etc/letsencrypt/live/certfolder/privkey.pem /app/ssl/key.pem
